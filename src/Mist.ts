8b9f6af0b56a7815f4241856e7caad8fe917827a
Ronie Meque <rmeque@endeavorstreaming.com>
2023-09-13 09:59:30 +0100

    feat: support detection of file type (#19)
    * feat: support detection of file type
    
    * feat: apply same adjustment for threaded mist
    
    * chore: adjust version

--- src/Mist.ts
+++ src/Mist.ts
@@ -13,6 +13,7 @@ export default class Mist extends MistBase {
 					name,
 					file,
 					progress: (percentage) => this.onProgress(percentage, file.id),
+					contentType: config.sendFileContentType ? file.type : undefined, // if not specified or affected by external factors, the content-type of the file in the bucket will be `application/octet-stream`. The `sendFileContentType` config keeps this backwards compatible.
 				};
 				const cancel = () => evaporate.cancel(`${data.bucketName}/${data.path}`);
 
